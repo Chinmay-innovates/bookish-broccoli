711718234ca4dfb23af13a3ecbadfd226665a6be
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-06 00:39:23 +0530

    set up order-service

--- order-service/src/main/java/com/microservices/order/web/exception/GlobalExceptionHandler.java
+++ order-service/src/main/java/com/microservices/order/web/exception/GlobalExceptionHandler.java
@@ -1,6 +1,13 @@
 package com.microservices.order.web.exception;
 
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+import com.microservices.order.domain.OrderNotFoundException;
 import java.net.URI;
+import java.time.Instant;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -9,4 +16,26 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     private static final URI NOT_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/not-found");
     private static final URI ISE_FOUND_TYPE = URI.create("https://api.bookstore.com/errors/server-error");
     private static final String SERVICE_NAME = "catalog-service";
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail handleUnhandledException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setType(ISE_FOUND_TYPE);
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    ProblemDetail handleOrderNotFoundException(OrderNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(NOT_FOUND, e.getMessage());
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setTitle("Order Not Found");
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
 }
