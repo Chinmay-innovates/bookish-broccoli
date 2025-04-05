1f2d568ef97ad88f961a3eba7dd8dec206e0176d
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-05 11:55:18 +0530

    initialize catalog service

---
+++ catalog-service/src/test/java/com/microservices/catalog/CatalogServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.microservices.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class
+CatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
