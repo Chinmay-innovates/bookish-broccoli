fa7cee3d31c3c230aaa3edda40538960175581ad
Chinmay-innovates <chinmaygope69@gmail.com>
2025-04-06 13:12:00 +0530

    RabbitMQ Integration Setup

---
+++ order-service/src/main/java/com/microservices/order/web/controller/RabbitMQController.java
@@ -0,0 +1,34 @@
+package com.microservices.order.web.controller;
+
+import com.microservices.order.ApplicationProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+ class RabbitMQController {
+    private final RabbitTemplate rabbitTemplate;
+    private final ApplicationProperties properties;
+
+    RabbitMQController(RabbitTemplate rabbitTemplate, ApplicationProperties properties) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.properties = properties;
+    }
+
+    @PostMapping("/send")
+    public void sendMessage(@RequestBody MyMessage message) {
+        rabbitTemplate.convertAndSend(
+                properties.orderEventsExchange(),
+                message.routingKey(),
+                message.payload()
+        );
+    }
+
+}
+
+record MyMessage(String routingKey, MyPayload  payload) {
+}
+
+record MyPayload(String content) {
+}
